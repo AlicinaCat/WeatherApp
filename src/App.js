b9caa9d661c7a4b7f009745f40f5cf9075326e4b
alicinacat <alice.casaburi@gmail.com>
2019-03-20 16:22:57 +0100

    transferred weather functionality to its own component

--- src/App.js
+++ src/App.js
@@ -4,52 +4,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Weather from './components/Weather';
 import Titles from './components/Titles';
-import Form from './components/Form';
-
-const API_KEY = '33b94a1d1da5daf4a1643aefc11f0c99';
 
 class App extends Component {
   // initial state
-  state = {
-    temperature: undefined,
-    city: undefined,
-    country: undefined,
-    humidity: undefined,
-    description: undefined,
-    error: undefined
-  }
-
-  getWeather = async (e) => {
-    e.preventDefault();
-
-    const city = e.target.elements.city.value;
-    const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
-    const data = await api_call.json();
-
-    // if values are not null
-    if (city && country) {
-      console.log(data);
-
-      this.setState({
-        temperature: data.main.temp,
-        city: data.name,
-        country: data.sys.country,
-        humidity: data.main.humidity,
-        description: data.weather[0].description,
-        error: ""
-      });
-    } else {
-      this.setState({
-        temperature: undefined,
-        city: undefined,
-        country: undefined,
-        humidity: undefined,
-        description: undefined,
-        error: "Please enter location."
-      });
-    }
-  }
+  
 
   render() {
     return (
@@ -63,15 +21,7 @@ class App extends Component {
                 </div>
 
                 <div className="col form-container">
-                  <Form getWeather={this.getWeather} />
-                  <Weather
-                    temperature={this.state.temperature}
-                    city={this.state.city}
-                    country={this.state.country}
-                    humidity={this.state.humidity}
-                    description={this.state.description}
-                    error={this.state.error}
-                  />
+                  <Weather />
                 </div>
               </div>
             </div>
@@ -81,16 +31,5 @@ class App extends Component {
     );
   }
 }
-
-// <Titles />
-// <Form getWeather={this.getWeather} />
-// <Weather
-//   temperature={this.state.temperature}
-//   city={this.state.city}
-//   country={this.state.country}
-//   humidity={this.state.humidity}
-//   description={this.state.description}
-//   error={this.state.error}
-// />
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,36 +1,67 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Form from './Form';
 
-// stateless component
+const API_KEY = '33b94a1d1da5daf4a1643aefc11f0c99';
 
-const Weather = (props) => (
-// { 
-            // return (
+export default class Weather extends Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        tomorrow: undefined,
+        error: undefined
+      }
+    
+      getWeather = async (e) => {
+        e.preventDefault();
+    
+        const city = e.target.elements.city.value;
+        const country = e.target.elements.country.value;
+        const api_call_weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+        const api_call_forecast = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+        const dataToday = await api_call_weather.json();
+        const dataForecast = await api_call_forecast.json();
+    
+        // if values are not null
+        if (city && country) {
+          console.log(dataToday);
+          console.log(dataForecast);
+    
+          this.setState({
+            temperature: dataToday.main.temp,
+            city: dataToday.name,
+            country: dataToday.sys.country,
+            humidity: dataToday.main.humidity,
+            description: dataToday.weather[0].description,
+            tomorrow: dataForecast.list[0].main.temp,
+            error: ""
+          });
+        } else {
+          this.setState({
+            error: "Please enter location."
+          });
+        }
+      }
+
+    render() {
+        return (
             <div>
-                { props.city && props.country && <p>City: {props.city}, Country: {props.country}</p>}
-                { props.temperature && <p>Temp: {props.temperature}</p>}
-                { props.humidity && <p>Hum: {props.humidity}</p> }
-                { props.description && <p>Descr: {props.description}</p> }
-                { props.error && <p>{props.error}</p> }
+                <Form getWeather={this.getWeather} />
+                { this.state.city && this.state.country && <p>City: {this.state.city}, Country: {this.state.country}</p>}
+                { this.state.temperature && <p>Temp: {this.state.temperature}</p>}
+                { this.state.humidity && <p>Hum: {this.state.humidity}</p> }
+                { this.state.description && <p>Descr: {this.state.description}</p> }
+                { this.state.error && <p>{this.state.error}</p> }
             </div>
-);
-// }
+        );
+    }
+}
+   
 
-export default Weather;
 
-// class component
 
-// export default class Weather extends Component {
-//     render() {
-//         return (
-//             <div>
-//                 { this.props.city && this.props.country && <p>City: {this.props.city}, Country: {this.props.country}</p>}
-//                 { this.props.temperature && <p>Temp: {this.props.temperature}</p>}
-//                 { this.props.humidity && <p>Hum: {this.props.humidity}</p> }
-//                 { this.props.description && <p>Descr: {this.props.description}</p> }
-//                 { this.props.error && <p>{this.props.error}</p> }
-//             </div>
-//         );
-//     }
-// }
-    // const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=33b94a1d1da5daf4a1643aefc11f0c99');
-        
+
+// const api_call = await fetch('http://api.openweathermap.org/data/2.5/weather?q=Stockholm&APPID=33b94a1d1da5daf4a1643aefc11f0c99');
+
