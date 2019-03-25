8f9496c91f240b8da679660cf0944286105a71c1
alicinacat <alice.casaburi@gmail.com>
2019-03-25 10:13:26 +0100

    added functionality to favorite list so that it loads the actual weather on click

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Form from './Form';
 import Forecast from './Forecast';
+import { NavLink } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 const API_KEY = '33b94a1d1da5daf4a1643aefc11f0c99';
 
@@ -57,11 +59,44 @@ export default class Weather extends Component {
     }
   }
 
+  getWeatherFavorite = async (e) => {
+    e.preventDefault();
+
+    localStorage.setItem("city", "stockholm");
+    localStorage.setItem("country", "se");
+
+    console.log(this.state);
+
+
+    const target = "" + e.target;
+    console.log(target);
+
+    const favorite = target.substr(22);
+    const name = favorite.split(':');
+    const city = name[0];
+    const country = name[1];
+
+    console.log(city + ', ' + country);
+
+    const api_call_weather = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
+    const dataToday = await api_call_weather.json();
+
+    this.setState({
+      favorites: [],
+      icon: dataToday.weather[0].main,
+      temperature: Math.round(dataToday.main.temp),
+      city: dataToday.name,
+      country: dataToday.sys.country,
+      description: dataToday.weather[0].description,
+      error: "",
+    });
+  }
+
   addToFavorites = () => {
     if (localStorage.getItem("favorites") === null) {
       let favorites = [];
       localStorage.setItem("favorites", JSON.stringify(favorites));
-      this.setState({state: this.state});
+      this.setState({ state: this.state });
     }
     else {
       let favorites = localStorage.getItem('favorites');
@@ -69,9 +104,9 @@ export default class Weather extends Component {
       favorites.push(this.state.city + ":" + this.state.country);
       console.log(favorites);
       localStorage.setItem("favorites", JSON.stringify(favorites));
-      this.setState({state: this.state});
+      this.setState({ state: this.state });
     }
-    
+
   }
 
   render() {
@@ -81,38 +116,38 @@ export default class Weather extends Component {
     console.log(array);
     var favoriteList;
     if (array != null) {
-        favoriteList = array.map((city) => {
-            let name = city.split(':');
-            return <li>{name[0]}, {name[1]}</li>
-        });
+      favoriteList = array.map((city) => {
+        let name = city.split(':');
+        return <li><a onClick={this.getWeatherFavorite} href={'/' + name[0] + ':' + name[1]}>{name[0]}, {name[1]}</a></li>
+      });
     }
 
 
     return (
-      <div>
-      <div>
-      <h1>Favorites</h1>
-      <p>List of cities</p>
-      {this.state.favorites && <ul>{favoriteList}</ul>}
-    </div>
-      <div>
-        <Form getWeather={this.getWeather} />
-        {/* { this.state.icon && <img src={this.state.icon} alt={this.state.icon} />} */}
-        {this.state.city && this.state.country && this.state.icon === "Clear" && <img src={require('../pics/Clear.png')} alt="clear" />}
-        {this.state.city && this.state.country && this.state.icon === "Clouds" && <img src={require('../pics/Clouds.png')} alt="clouds" />}
-        {this.state.city && this.state.country && this.state.icon === "Rain" && <img src={require('../pics/Rain.png')} alt="rain" />}
-        {this.state.city && this.state.country && this.state.icon === "Snow" && <img src={require('../pics/Snow.png')} alt="snow" />}
-        {this.state.city && this.state.country && this.state.icon === "Mist" && <img src={require('../pics/Mist.png')} alt="mist" />}
+      <div className="row">
+        <div className="col form-container">
+          <Form getWeather={this.getWeather} />
+          {/* { this.state.icon && <img src={this.state.icon} alt={this.state.icon} />} */}
+          {this.state.city && this.state.country && this.state.icon === "Clear" && <img src={require('../pics/Clear.png')} alt="clear" />}
+          {this.state.city && this.state.country && this.state.icon === "Clouds" && <img src={require('../pics/Clouds.png')} alt="clouds" />}
+          {this.state.city && this.state.country && this.state.icon === "Rain" && <img src={require('../pics/Rain.png')} alt="rain" />}
+          {this.state.city && this.state.country && this.state.icon === "Snow" && <img src={require('../pics/Snow.png')} alt="snow" />}
+          {this.state.city && this.state.country && this.state.icon === "Mist" && <img src={require('../pics/Mist.png')} alt="mist" />}
 
 
-        {this.state.city && this.state.country && <p>City: {this.state.city}, Country: {this.state.country}
-          <button onClick={this.addToFavorites}>Add to fav</button></p>}
-        {this.state.temperature && <p>Temp: {this.state.temperature}°</p>}
-        {this.state.description && <p>Descr: {this.state.description}</p>}
-        {this.state.error && <p>{this.state.error}</p>}
-        {this.state.city && <Forecast city={this.state.city} country={this.state.country} />}
+          {this.state.city && this.state.country && <p>City: {this.state.city}, Country: {this.state.country}
+            <button onClick={this.addToFavorites}>Add to fav</button></p>}
+          {this.state.temperature && <p>Temp: {this.state.temperature}°</p>}
+          {this.state.description && <p>Descr: {this.state.description}</p>}
+          {this.state.error && <p>{this.state.error}</p>}
+          {this.state.city && <Forecast city={this.state.city} country={this.state.country} />}
 
-      </div>
+        </div>
+        <div className="col form-container">
+          <h1>Favorites</h1>
+          <p>List of cities</p>
+          {this.state.favorites && <ul>{favoriteList}</ul>}
+        </div>
       </div>
     );
   }
